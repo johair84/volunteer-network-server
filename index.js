96774949ea0c1e857b68536df70151d293b6e0e9
johair84 <33111357+johair84@users.noreply.github.com>
2020-10-10 22:32:31 +0600

    upadte databse name

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 
 
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7mufj.mongodb.net/${process.env.DB_Name}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7mufj.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const registrations = client.db("volunteerNetwork").collection("registrations");
